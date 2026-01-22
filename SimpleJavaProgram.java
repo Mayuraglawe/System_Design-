126c6a471c888c9e97ce65d3a185f621d2213904
Mayuraglawe <mayuraglawe06@gmail.com>
2026-01-22 23:02:56 +0530

    Add Simple java program

---
+++ SimpleJavaProgram.java
@@ -0,0 +1,12 @@
+public class SimpleJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+
+        // Simple calculation example
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
